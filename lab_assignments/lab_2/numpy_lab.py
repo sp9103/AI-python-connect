365752551561fe34c45cce83aa94e3d92855e012
sp9103 <moonsp0304@gmail.com>
2019-04-19 18:20:03 +0900

    Update numpy_lab.py

--- lab_assignments/lab_2/numpy_lab.py
+++ lab_assignments/lab_2/numpy_lab.py
@@ -2,36 +2,57 @@ import numpy as np
 
 
 def n_size_ndarray_creation(n, dtype=np.int):
-    pass
-
+    return np.array(range(n*n), dtype=dtype).reshape((n, n))
 
 def zero_or_one_or_empty_ndarray(shape, type=0, dtype=np.int):
-    pass
-
+    if type == 0:
+        return np.zeros(shape, dtype=dtype)
+    elif type == 1:
+        return np.ones(shape, dtype=dtype)
+    else:
+        return np.empty(shape, dtype=dtype)
 
 def change_shape_of_ndarray(X, n_row):
-    pass
+    new_col = int(X.size / n_row)
+    return X.reshape((n_row, new_col))
 
 
 def concat_ndarray(X_1, X_2, axis):
-    pass
+    try:
+        result = np.concatenate((X_1, X_2), axis)
+    except:
+        return False
+    return result
 
 
 def normalize_ndarray(X, axis=99, dtype=np.float32):
-    pass
+    X = X.astype(dtype)
+    if axis == 99:
+        x_mean = np.mean(X)
+        x_std = np.std(X)
+    elif axis == 0:
+        x_mean = np.mean(X, axis=0).reshape(1, -1)
+        x_std = np.std(X, axis=0).reshape(1, -1)
+    else:
+        x_mean = np.mean(X, axis=1).reshape(-1, 1)
+        x_std = np.std(X, axis=1).reshape(-1, 1)
+
+    Z = (X - x_mean) / x_std
+    return Z
 
 
 def save_ndarray(X, filename="test.npy"):
-    pass
+    np.save(filename, X)
 
 
 def boolean_index(X, condition):
-    pass
+    return X[eval(str("X") + condition)]
 
 
 def find_nearest_value(X, target_value):
-    pass
+    return X[np.argmin(np.abs(X-target_value))]
 
 
 def get_n_largest_values(X, n):
-    pass
+    # 확인 필요함
+    return X[np.argsort(X[::-1])[:n]]
